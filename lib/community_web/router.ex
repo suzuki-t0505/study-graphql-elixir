57050e84f9c47d905d8d2f70fbbdcd8d70e8f963
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-22 16:51:09 +0900

    :sparkles: GraphQLのスキーマの定義とクエリリゾルバーの作成

--- lib/community_web/router.ex
+++ lib/community_web/router.ex
@@ -5,6 +5,15 @@ defmodule CommunityWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/" do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.Endpoint}
+  end
+
   scope "/api", CommunityWeb do
     pipe_through :api
   end
