e6c16c7bafedf0312ef5698ae40f6c2184b80bf0
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-06-22 16:37:38 +0900

    :sparkles: linksテーブルの追加

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,12 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Community.Repo.insert!(%Community.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Community.News.Link
+alias Community.Repo
+
+
+
+links =
+  [
+    %Link{url: "http://graphql.org/", description: "The Best Query Language"},
+    %Link{url: "http://dev.apollodata.com/", description: "Awesome GraphQL Client"}
+  ]
+
+Enum.each(links, fn link -> Repo.insert!(link) end)
